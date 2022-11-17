f1ff8be09442e57c6177a4cb4f10dfaa0d6bd4b1
zacjjohnson <wecooked@gmail.com>
2022-11-16 22:52:06 -0500

    search and filter

--- src/App.js
+++ src/App.js
@@ -36,7 +36,17 @@ class App extends Component {
     console.log('Render');
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {console.log(event)}} />
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
+          console.log(event.target.value)
+          const searchString = event.target.value.toLowerCase();
+          const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLowerCase().includes(searchString);
+          })
+
+          this.setState(() => {
+            return { monsters: filteredMonsters }
+          })
+          }} />
         {
           this.state.monsters.map((monster) => {
             return <div key={monster.id}><h1>{monster.name} </h1></div>
